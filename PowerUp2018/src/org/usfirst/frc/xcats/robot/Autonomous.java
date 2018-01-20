2b6db6cfdccff7165b21335b413da2c9eaa691a6
railroad890 <jamestrains890@gmail.com>
2018-01-20 14:13:50 -0500

    added game data string and assigned it to the game specific data from the fms

--- PowerUp2018/src/org/usfirst/frc/xcats/robot/Autonomous.java
+++ PowerUp2018/src/org/usfirst/frc/xcats/robot/Autonomous.java
@@ -64,6 +64,7 @@ public class Autonomous {
 	String _autoSelected;
 	String _defenseSelected;
 	String _shooterModeSelected;
+	String _gameData;
 	SendableChooser _defensePosition;
 	SendableChooser _gearProcessing;
 
@@ -198,6 +199,7 @@ public class Autonomous {
 		_steps =  new ArrayList<AutonomousStep>();
 		
 		boolean blueAlliance = false;
+		_gameData = DriverStation.getInstance().getGameSpecificMessage();
 		
 		if (DriverStation.getInstance().getAlliance() == DriverStation.Alliance.Blue){
 			blueAlliance = true;

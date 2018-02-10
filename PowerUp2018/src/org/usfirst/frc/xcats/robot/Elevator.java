a663e958cc6d12e8294aa5b41b366ab016f5b39f
railroad890 <jamestrains890@gmail.com>
2018-02-10 11:28:22 -0500

    re-enabled second motor for elevator

--- PowerUp2018/src/org/usfirst/frc/xcats/robot/Elevator.java
+++ PowerUp2018/src/org/usfirst/frc/xcats/robot/Elevator.java
@@ -6,12 +6,12 @@ import org.usfirst.frc.xcats.robot.XCatsSpeedController.SCType;
 public class Elevator {
 	
 	private XCatsSpeedController _elevatorMaster;
-	//private XCatsSpeedController _elevatorFollower;
+	private XCatsSpeedController _elevatorFollower;
 	
 	public Elevator() {
 		_elevatorMaster = new XCatsSpeedController("Elevator", Enums.ELEVATOR_MASTER_CAN_ID, true, SCType.TALON, null, null);
-		//_elevatorFollower = new XCatsSpeedController("Elevator", Enums.ELEVATOR_FOLLOWER_CAN_ID, true, SCType.TALON, null, null);
-		//_elevatorFollower.setFollower(Enums.ELEVATOR_MASTER_CAN_ID);
+		_elevatorFollower = new XCatsSpeedController("Elevator", Enums.ELEVATOR_FOLLOWER_CAN_ID, true, SCType.TALON, null, null);
+		_elevatorFollower.setFollower(Enums.ELEVATOR_MASTER_CAN_ID);
 	}
 	
 	public void raise() {
